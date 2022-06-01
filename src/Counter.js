c6b71702cfd7de40d2b87c526062c353026abd3d
Jasterix <jasterix@users.noreply.github.com>
2022-06-01 15:11:32 -0400

    working with Counter as a functional component

--- src/Counter.js
+++ src/Counter.js
@@ -2,17 +2,16 @@ import { connect } from "react-redux";
 import React from "react";
 import { increment, decrement } from "./actions";
 
-class Counter extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>count: {this.props.count}</h1>
-        <button onClick={() => this.props.increment()}> increment +1</button>
-        <button onClick={() => this.props.decrement()}> decrement -1</button>
-      </>
-    );
-  }
+function Counter(props) {
+  return (
+    <>
+      <h1>count: {props.count}</h1>
+      <button onClick={() => props.increment()}> increment +1</button>
+      <button onClick={() => props.decrement()}> decrement -1</button>
+    </>
+  );
 }
+
 const mapStateToProps = (state) => {
   return {
     count: state.count
