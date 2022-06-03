0560daffcd69422b32313d63a55bdbfb5f901a56
Loune Calixte <lounecalixte@microsoft.com>
2022-06-03 14:52:57 -0400

    Add redux-logger for logging actions, add applymiddleware

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import React, { Component } from "react";
 import Counter from "./Counter";
 import { Provider } from "react-redux";
 import counterReducer from "./counterReducer";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 
-const store = createStore(counterReducer, composeWithDevTools());
+const store = createStore(
+	counterReducer,
+	composeWithDevTools(applyMiddleware(logger))
+);
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="App">
-          <h1>Hello CodeSandbox</h1>
-          <h2>Start editing to see some magic happen!</h2>
-          <Counter />
-        </div>
-      </Provider>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<div className="App">
+					<h1>Hello CodeSandbox</h1>
+					<h2>Start editing to see some magic happen!</h2>
+					<Counter />
+				</div>
+			</Provider>
+		);
+	}
 }
 
 export default App;
